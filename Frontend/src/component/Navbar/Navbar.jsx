cd74287634b61a6ed5b166206e004c53c1336eab
Phetar <phetarmoses05@gmail.com>
2024-07-11 13:17:07 +0100

    my first commit

---
+++ Frontend/src/component/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../Assets/Ecommerce_Assets/logo.png'
+
+export const Navbar = () => {
+  return (
+    <div classname='navbar'>
+       
+    </div>
+  )
+}
